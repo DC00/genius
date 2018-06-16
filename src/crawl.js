cf88a0d6903811b6a0074d1afb4a76121d133ec0
DC00 <djc4ku@virginia.edu>
2018-06-15 23:44:49 -0400

    async stuff

--- src/crawl.js
+++ src/crawl.js
@@ -10,45 +10,59 @@ const Promise = require('bluebird')
 const Artist = require('./artist.js')
 var config = require('./config.json')
 
-let scrape = async () => {
-  const browser = await puppeteer.launch({headless : false})
-  const page = await browser.newPage()
+async function scrape() {
+  const browser = await puppeteer.launch({headless : false}).catch(err => { console.log(err) })
+  const page = await browser.newPage().catch(err => { console.log(err) })
   var data
+
+  /*
+  await page.goto(config.genius_root + "/Eminem").catch(err => { console.log(err) })
+  let html = await page.content().catch(err => { console.log(err) })
+  let $ = cheerio.load(html)
+  console.log($('.profile_tabs-tab-count').text())
+  */
 
   for (var i = 1; i < config.max_page; i++) {
     console.log(config.genius_url + i)
-    await page.goto(config.genius_url + i)
-    let html = await page.content()
+    await page.goto(config.genius_url + i).catch(err => { console.log(err) })
+    let html = await page.content().catch(err => { console.log(err) })
     let $ = cheerio.load(html)
-    data = $('.badge_container').map(function(inx, badge) {
+    data = $('.badge_container').map(async function(inx, badge) {
       var name = $(badge).find('[data-id]').text().trim()
       var iq = $(badge).find('.iq').text().trim()
       var url = $(badge).find('a').attr('href')
-      // follow url to get followers and annotations
-      return {name, iq, url}
+      var followers = async function() {
+        try {
+          await artistPage.goto(config.genius_root + url, { waitUntil: "load" }).catch(err => { console.log(err) })
+          let html2 = await artistPage.content().catch(err => { console.log(err) })
+          let $ = await cheerio.load(html2)
+          var f = $('.profile_tabs-tab-count').text()
+          return f
+
+        } catch (e) {
+          console.log(e)
+        }
+        /* let artistPage = await browser.newPage()
+        await artistPage.goto(config.genius_root + url, { waitUntil: "load" }).catch(err => { console.log(err) })
+        let html2 = await artistPage.content().catch(err => { console.log(err) })
+        let $ = await cheerio.load(html2)
+        var f = $('.profile_tabs-tab-count').text()
+        */
+      }
+      return {name, iq, url, followers}
     }).get()
   }
 
-  await browser.close()
+  await browser.close().catch(err => { console.log(err) })
   return data
-
 }
 
-let fanotations = async (data) => {
-  const browser = await puppeteer.launch({headless : false})
-  const page = await browser.newPage()
-  // forEach lets you mutate as you iterate, map() returns a new array
-  data.forEach(function(json) {
-    await page.goto(config.genius_root + json.url)
+// scrape() is async, returns Promise
+scrape()
+  .then(data => { console.log(data) })
+  .catch(err => { console.log(err) })
 
-  })
-}
 
-// scrape() is async, returns Promise. resolved promise sends data to getFanotations
-/*
-scrape().then((data) => {
-  await getFanotations(data)
-})
-*/
 
-scrape().then(getFanotations(data))
+
+
