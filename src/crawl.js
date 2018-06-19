4e82ee37356129882126a92a358b763b9db21aa6
DC00 <djc4ku@virginia.edu>
2018-06-19 00:04:10 -0400

    select

--- src/crawl.js
+++ src/crawl.js
@@ -10,35 +10,65 @@ const Promise = require('bluebird')
 const Artist = require('./artist.js')
 var config = require('./config.json')
 
+function getAnnotations() {
+
+
+}
+
+async function scrapeInfiniteScroll(page, getAnnotations, maxCount, delay=1000) {
+  // click 'all contributions' dropdown, select annotations
+  let dropdown = document.querySelectorAll('body > routable-page > ng-outlet > routable-profile-page > ng-outlet > routed-page > profile-page > div.column_layout > div.column_layout-column_span.column_layout-column_span--primary > div > div > div > select-dropdown > div > div:nth-child(2)')
+  console.log(dropdown)
+  let annotations
+  try {
+  while (annotations < maxCount) {
+    annotations = await page.evaluate(getAnnotations)
+
+    }
+
+  } catch (err) {
+      console.log(err)
+  }
+  return 
+}
+
 async function scrape() {
-  process.setMaxListeners(100);
-  const browser = await puppeteer.launch({headless : false})
+  process.setMaxListeners(100)
+  const browser = await puppeteer.launch({
+    headless : false,
+    slowMo: 250
+  })
   const page = await browser.newPage()
   var data
 
+  await page.goto(config.genius_root + '/Eminem')
+  var annotations = await scrapeInfiniteScroll(page, getAnnotations, 1000, 1000)
+
+  /*
   for (var i = 1; i < config.max_page; i++) {
     await page.goto(config.verified_artists_url + i)
-    let html = await page.content()
-    let $ = cheerio.load(html)
+    const html = await page.content()
+    const $ = cheerio.load(html)
 
     data = await Promise.all($('.badge_container').map(async function(inx, badge) {
-      var name = $(badge).find('[data-id]').text().trim()
-      var iq = $(badge).find('.iq').text().trim()
-      var url = $(badge).find('a').attr('href')
+      const name = $(badge).find('[data-id]').text().trim()
+      const iq = $(badge).find('.iq').text().trim()
+      const url = $(badge).find('a').attr('href')
       await page.goto(config.genius_root + url)
-      var followers = await page.evaluate((selector) => {
+      const followers = await page.evaluate((selector) => {
         console.log(document.getElementsByClassName(selector))
         return document.querySelector(selector).innerText
       }, config.follower_sel)
 
-      // get annotations on same page
-      // infinite scrolling bullshit
+      var annotations = await scrapeInfiniteScroll(page, getAnnotations, maxCount, 1000)
+
 
       return {name, iq, url, followers}
     }).get())
   }
+  */
 
-  browser.close()
+  await browser.close()
   return data
 }
 
