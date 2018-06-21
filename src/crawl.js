23aa6babc55e697967add73647a41b1ff9003a18
DC00 <djc4ku@virginia.edu>
2018-06-20 23:59:32 -0400

    crawl

--- src/crawl.js
+++ src/crawl.js
@@ -10,44 +10,6 @@ const Promise = require('bluebird')
 const Artist = require('./artist.js')
 var config = require('./config.json')
 
-
-
-function getAnnotations() {
-  return 100
-}
-
-async function scrapeInfiniteScroll(page, getAnnotations, maxCount, delay=1000) {
-  // click 'all contributions' dropdown, select annotations
-
-  var element = await page.evaluate(() => {
-    return findElemByText({str: "All Contributions"})[0].click()
-  })
-
-  let annotations
-  try {
-    while (annotations < maxCount) {
-      annotations = await page.evaluate(getAnnotations)
-    }
-  } catch (err) {
-      console.log(err)
-  }
-  return annotations
-}
-
-function findElemByText({str, selector = '*', leaf = 'outerHTML'}) {
-  // generate regex from string
-  const regex = new RegExp(str, 'gmi');
-
-  // search the element for specific word
-  const matchOuterHTML = e => (regex.test(e[leaf]))
-
-  // array of elements
-  const elementArray = [...document.querySelectorAll(selector)];
-
-  // return filtered element list
-  return elementArray.filter(matchOuterHTML)
-}
-
 async function scrape() {
   process.setMaxListeners(100)
   const browser = await puppeteer.launch({
@@ -56,8 +18,15 @@ async function scrape() {
   const page = await browser.newPage()
   var data
 
-  await page.goto(config.genius_root + '/Eminem')
-  var annotations = await scrapeInfiniteScroll(page, getAnnotations, 1000, 1000)
+  await page.goto(config.genius_root + '/Eminem', {waitUntil: "networkidle2"})
+  // var annotations = await scrapeInfiniteScroll(page, getAnnotations, 1000, 1000)
+
+  const html = await page.content()
+  const $ = cheerio.load(html)
+  console.log($('body > routable-page > ng-outlet > routable-profile-page > ng-outlet > routed-page > profile-page > div.column_layout > div.column_layout-column_span.column_layout-column_span--primary > div > div > div > select-dropdown > div > div:nth-child(2)').text())
+  await page.click('body > routable-page > ng-outlet > routable-profile-page > ng-outlet > routed-page > profile-page > div.column_layout > div.column_layout-column_span.column_layout-column_span--primary > div > div > div > select-dropdown > span')
+  data = 10
+
 
   /*
   for (var i = 1; i < config.max_page; i++) {
@@ -83,7 +52,7 @@ async function scrape() {
   }
   */
 
-  await browser.close()
+  // await browser.close()
   return data
 }
 
